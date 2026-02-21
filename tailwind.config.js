d3049e47473798732de7d1b54259a98175eb28b7
krnkiran22 <kirandev2210@gmail.com>
2026-02-22 01:58:38 +0530

    setting up the project structure and configs

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lime: "#A3FF12",
+      },
+      fontFamily: {
+        orbitron: ["Orbitron", "sans-serif"],
+        rajdhani: ["Rajdhani", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
